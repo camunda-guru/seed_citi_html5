fac1a0681e17c600ce45b5f19f07840ceebf6f5f
eswaribala <Parameswaribala@gmail.com>
2017-08-02 15:58:18 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,90 +1,37 @@
 /**
- * Created by BALASUBRAMANIAM on 31-07-2017.
+ * Created by BALASUBRAMANIAM on 01-12-2015.
  */
-/*
-document.getElementById("firstName").oninvalid = function () {
-    this.setCustomValidity(this.value ? '' : 'My message');
+var webSocket =
+    new WebSocket('ws://localhost:7070/SocketCommn/SocketDemo');
+
+webSocket.onerror = function(event) {
+    onError(event)
 };
-input.addEventListener('invalid', function(e) {
-    if(input.validity.valueMissing){
-        e.target.setCustomValidity("PLZ CREATE A USERNAME, YO!");
-    } else if(!input.validity.valid) {
-        e.target.setCustomValidity("U R DOIN IT WRONG!");
-    }
-}, false);
-*/
 
-function test()
-{
-    console.log("Testing....");
-}
-$(function () {
-    $("#city").autocomplete({
-        source: function (request, response) {
-            $.ajax({
-                url: "http://gd.geobytes.com/AutoCompleteCity?callback=?&q="+request.term,
-                type: "GET",
-                contentType: 'application/json; charset=utf-8',
-                dataType: "jsonp",
-                success: function (data) {
-                    response(data);
-                    $.each(data, function (item, value) {
-                        console.log(value);
-                    });
-                },
-                error:function(msg)
-                {
-                    console.log(msg);
-                }
-            });
-        }
-    });
-});
+webSocket.onopen = function(event) {
+    onOpen(event)
+};
 
-function save()
-{
-   var fileRef= document.getElementById("photo");
-    console.log(fileRef.files.length);
-    var filePattern=/image.*/;
-    for(i=0;i<fileRef.files.length;i++)
-    {
-        if(fileRef.files[i].type.match(filePattern))
-            storeImage(i,fileRef.files[i]);
+webSocket.onmessage = function(event) {
+    onMessage(event)
+};
 
-    }
-
-
+function onMessage(event) {
+    document.getElementById('messages').innerHTML
+        += '<br />' + event.data;
+    webSocket.send('received');
 }
 
-function storeImage(position, file)
-{
-    var fileReader =new FileReader();
-    fileReader.readAsDataURL(file);
-    fileReader.onload=function()
-    {
-        window.localStorage.setItem("Image"+position,fileReader.result);
-    }
-}
-for(i=0;i<window.localStorage.length;i++)
-{
-    if(window.localStorage.getItem("Image"+i)!=undefined)
-    {
-        displayPhoto(window.localStorage.getItem("Image"+i));
-    }
+function onOpen(event) {
+    document.getElementById('messages').innerHTML
+        = 'Connection established';
 }
 
-function displayPhoto(file)
-{
-    var img = new Image();
-    img.width=100;
-    img.height=100;
-    img.onload = function (evt) {
-        var photoref = document.getElementById("docs");
-        photoref.appendChild(img);
-        node = document.createElement("hr");
-        photoref.appendChild(node);
-    }
-
-    img.src = file;
+function onError(event) {
+    alert(event.data);
 }
 
+function start() {
+    webSocket.send('hello');
+    return false;
+}
